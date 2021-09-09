efe454ceae980a4e99ae320e0a903f9af2f903e5
Apollo Tang <tang.apollo@gmail.com>
2021-09-09 16:18:22 -0400

    [2c] remove app--node-2 to simplify investigation

--- 02c-node-apps-w-rig--zulip-question-experiment/apps/app--node-2/src/bar/index.ts
+++
@@ -1,7 +0,0 @@
-import {fnInFoo1} from "@myscope/util--foo-1"
-
-const someString:string = fnInFoo1(23)
-
-export {
-  someString
-}
--- 02c-node-apps-w-rig--zulip-question-experiment/apps/app--node-2/src/start.ts
+++
@@ -1,4 +0,0 @@
-console.log('This is node-app-2')
-
-import {someString} from './bar/index'
-console.log('someString', someString)
